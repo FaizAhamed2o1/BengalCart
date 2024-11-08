9ce8bf43cdac97bc418087912f1363b397270587
Sakib <56402127+sakibovi123@users.noreply.github.com>
2024-11-08 15:18:57 +0600

    Frontend config done

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+// resources/js/app.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>HELLO BENGAL CART</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
