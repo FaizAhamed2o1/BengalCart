363796e1350aa364500b2caea64970158f701ece
Sakib <sakibovi123@gmail.com>
2024-11-16 21:58:33 +0600

    updated

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    
 }
