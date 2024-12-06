b3e2977c0a39ce008c2084954c0d994aa6e7a198
Sakib <sakibovi123@gmail.com>
2024-12-06 11:53:38 +0600

    app name dynamic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HELLO WORLD</title>
+    <title>{{ env('APP_NAME') }}</title>
     @vite('resources/js/app.jsx')
 </head>
 <body>
