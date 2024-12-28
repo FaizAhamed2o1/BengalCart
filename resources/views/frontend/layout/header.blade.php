bf3e71de28ec6926aa3dd2c1cafeff527d2e5db1
Rifat Ahammed Tanim <138393708+Rifat-Ahammed-Tanim@users.noreply.github.com>
2024-12-29 01:48:01 +0600

    frontend done

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -42,11 +42,6 @@
                         <ul class="main-menu">
                             <li class="active-menu">
                                 <a href="{{route('home')}}">Home</a>
-                                <ul class="sub-menu">
-                                    <li><a href="index.html">Homepage 1</a></li>
-                                    <li><a href="home-02.html">Homepage 2</a></li>
-                                    <li><a href="home-03.html">Homepage 3</a></li>
-                                </ul>
                             </li>
 
                             <li>
@@ -158,35 +153,30 @@
 
             <ul class="main-menu-m">
                 <li>
-                    <a href="index.html">Home</a>
-                    <ul class="sub-menu-m">
-                        <li><a href="index.html">Homepage 1</a></li>
-                        <li><a href="home-02.html">Homepage 2</a></li>
-                        <li><a href="home-03.html">Homepage 3</a></li>
-                    </ul>
+                    <a href="{{route('home')}}">Home</a>
                     <span class="arrow-main-menu-m">
                         <i class="fa fa-angle-right" aria-hidden="true"></i>
                     </span>
                 </li>
 
                 <li>
-                    <a href="product.html">Shop</a>
+                    <a href="{{route('product')}}">Shop</a>
                 </li>
 
                 <li>
-                    <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
+                    <a href="{{route('shopping')}}" class="label1 rs1" data-label1="hot">Features</a>
                 </li>
 
                 <li>
-                    <a href="blog.html">Blog</a>
+                    <a href="{{route('blog')}}">Blog</a>
                 </li>
 
                 <li>
-                    <a href="about.html">About</a>
+                    <a href="{{route('about')}}">About</a>
                 </li>
 
                 <li>
-                    <a href="contact.html">Contact</a>
+                    <a href="{{route('contact')}}">Contact</a>
                 </li>
             </ul>
         </div>
